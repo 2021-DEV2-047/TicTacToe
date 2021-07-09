75d93f171e5c7d0406e9fb1a0cf37c9dac3e6927
2021-DEV2-047 <r1a2n2d3o4m@protonmail.com>
2021-07-09 21:27:57 +0200

    Change test with right index (array index begin at 0) - Create test_when_the_player_lines_up_three_symbols_horizontally_then_he_wins (failed test)

--- TicTacToeTests/TicTacToeTests.swift
+++ TicTacToeTests/TicTacToeTests.swift
@@ -7,27 +7,51 @@ class TicTacToeTests: XCTestCase {
   let ticTacToe = TicTacToe()
 
   func test_when_game_is_launched_then_the_board_is_initialized() {
+    // arrange & act
     let board = ticTacToe.board
+    // assert
     XCTAssertEqual(board.count, 9)
   }
   
   func test_when_game_started_then_cross_should_begin() {
-    ticTacToe.addSymbol(toBox: 1)
-    let testedBox = ticTacToe.getBox(1)
+    // arrange & act
+    ticTacToe.addSymbol(toBox: 0)
+    // assert
+    let testedBox = ticTacToe.getBox(0)
     XCTAssertEqual(testedBox, "X")
   }
   
   func test_when_player_has_played_then_symbol_change() {
+    // arrange
+    ticTacToe.addSymbol(toBox: 0)
+    // act
     ticTacToe.addSymbol(toBox: 1)
-    ticTacToe.addSymbol(toBox: 2)
-    let testedBox = ticTacToe.getBox(2)
+    // assert
+    let testedBox = ticTacToe.getBox(1)
     XCTAssertEqual(testedBox, "O")
   }
   
   func test_players_cannot_play_on_a_played_position() {
-    ticTacToe.addSymbol(toBox: 1)
-    ticTacToe.addSymbol(toBox: 1)
-    let testedBox = ticTacToe.getBox(1)
+    // arrange
+    ticTacToe.addSymbol(toBox: 0)
+    // act
+    ticTacToe.addSymbol(toBox: 0)
+    // assert
+    let testedBox = ticTacToe.getBox(0)
     XCTAssertEqual(testedBox, "X")
+  }
+  
+  func test_when_the_player_lines_up_three_symbols_horizontally_then_he_wins() {
+    // arrange
+    ticTacToe.addSymbol(toBox: 0) // X
+    ticTacToe.addSymbol(toBox: 3) // O
+    ticTacToe.addSymbol(toBox: 1) // X
+    ticTacToe.addSymbol(toBox: 4) // O
+    ticTacToe.addSymbol(toBox: 2) // X
+    // act
+    ticTacToe.addSymbol(toBox: 5)
+    // assert
+    let testedBox = ticTacToe.getBox(5)
+    XCTAssertEqual(testedBox, "")
   }
 }
