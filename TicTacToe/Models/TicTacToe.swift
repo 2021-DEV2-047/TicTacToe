81beef321c15b80dbdf6963607c36a3ae872dd6c
2021-DEV2-047 <r1a2n2d3o4m@protonmail.com>
2021-07-09 22:09:09 +0200

    - Pass diagonal test

--- TicTacToe/Models/TicTacToe.swift
+++ TicTacToe/Models/TicTacToe.swift
@@ -54,7 +54,8 @@ extension TicTacToe {
     let stringArray = currentSymbolBoxes.joined(separator: ",")
     let winningCombinations = [
       "0,1,2", "3,4,5", "6,7,8",
-      "0,3,6", "1,4,7", "2,5,8"
+      "0,3,6", "1,4,7", "2,5,8",
+      "0,4,8", "2,4,7"
     ]
     winningCombinations.forEach { (winningCombination) in
       if (stringArray.contains(winningCombination)) {
