16897c34031651560c81a90cadf550f9addb1d0f
2021-DEV2-047 <r1a2n2d3o4m@protonmail.com>
2021-07-12 02:42:06 +0200

    - fix Colors enum

--- TicTacToe/Common/Enums/Colors.swift
+++ TicTacToe/Common/Enums/Colors.swift
@@ -2,7 +2,7 @@ import Foundation
 import UIKit
 
 enum Colors {
-  static var lightGray = UIColor(red: 242, green: 242, blue: 242, alpha: 1.0)
-  static var blue = UIColor(red: 0, green: 0, blue: 153, alpha: 1.0)
-  static var red = UIColor(red: 204, green: 0, blue: 0, alpha: 1.0)
+  static var lightGray = UIColor(red: 242.0/255.0, green: 242.0/255.0, blue: 242.0/255.0, alpha: 1.0)
+  static var blue = UIColor(red: 0, green: 0, blue: 153.0/255.0, alpha: 1.0)
+  static var red = UIColor(red: 204.0/255.0, green: 0, blue: 0, alpha: 1.0)
 }
